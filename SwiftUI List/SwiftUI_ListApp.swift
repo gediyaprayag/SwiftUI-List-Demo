569e10c2683305762126b66361fcfbbd38286855
Prayag Gediya <prayaggediya60@gmail.com>
2022-05-27 22:05:51 +0530

    Initial Commit

---
+++ SwiftUI List/SwiftUI_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ListApp.swift
+//  SwiftUI List
+//
+//  Created by Prayag Gediya on 27/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
